6616657a87ce846e1c854288df979e3d3e474701
Mtmaj <majidimohamad1387@gmail.com>
2023-08-29 20:39:03 +0330

    news fixed

---
+++ api/v1/sellandrent/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const { body,validationResult } = require("express-validatore")
+const app = express.Router()
+
+app.get("/sell/get",(req,res)=>{
+    
+})
+
+module.exports.SellAndRent = app
