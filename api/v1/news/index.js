09e8ad78f631ee1b5b382af39bb9dc490cbf5df0
unknown <ejej4632@gmail.com>
2023-08-29 17:26:47 +0330

    create news api(update, delete)

--- api/v1/news/index.js
+++ api/v1/news/index.js
@@ -2,13 +2,48 @@ const express = require("express")
 const { NewsModel } = require("../../../models/news")
 const app = express.Router()
 const { auth_erorr } = require("../../../utils/request_massege")
+const { body,validationResult } = require("express-validator")
 
-app.get('/get',async (req,res)=>{
-    const News = await NewsModel.find()
-    return res.json(News.last())
+
+app.get("/new/get", async (req,res)=> {
+    const news_id = await News.findById(req.headers.new_id);
+    if(!new_id) 
+    return res.status(404)
+    .json({
+        "msg": "New not found"
+    })
+    res.json({data: news_id})
 })
 
-app.post("/add",async (req,res)=>{
+
+app.get("/news/getall", async (req,res)=>{
+    const pageNumber = req.headers.pageNumber;
+    const pageSise = 10;
+    const news = await NewsModel.find()
+    .skip((pageNumber - 1) * pageSise)
+    .limit(pageSise);
+    res.json({
+        data: news
+    })
+})
+/*
+        title : String,
+        date : Date,
+        code : Number,
+        images : [String],
+        text1 : String,
+        text2 : String
+*/
+
+app.post("/add",[
+    body("title","لطفا تایتل یا تیتر خبر خود را وارد کنید").notEmpty().isString(),
+    body("text1","لطفا متن 1 خود را وارد کنید").notEmpty().isString(),
+    body("text2","لطفا متن 2 خود را وارد کنید").notEmpty().isString()
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({data: null, errors: errors, message: 'Have Error',yourdata:req.body});
+    }
     if(req.headers.admin_auth == true){
         let news = new NewsModel({
             ...req.body
@@ -22,25 +57,35 @@ app.post("/add",async (req,res)=>{
     }
 })
 
-app.get("/new/get", async (req,res)=> {
-    const news_id = await News.findById(req.headers.new_id);
-    if(!new_id) 
-      return res.status(404)
-        .json({
-            "msg": "New not found"
+
+app.delete("/delete", async (req,res)=>{
+    const news = await NewsModel.findByIdAndRemove(req.headers.new_id);
+    if(!news){
+        return res.status(404).json({
+            "error": "the news not found"
         })
-    res.json({data: news_id})
+    }
 })
 
-app.get("/news/getall", async (req,res)=>{
-    const pageNumber = req.headers.pageNumber;
-    const pageSise = 10;
-    const news = await NewsModel.find()
-    .skip((pageNumber - 1) * pageSise)
-    .limit(pageSise);
+app.put("/update",[
+    body("title","لطفا تایتل یا تیتر خبر خود را وارد کنید").notEmpty().isString(),
+    body("text1","لطفا متن 1 خود را وارد کنید").notEmpty().isString(),
+    body("text2","لطفا متن 2 خود را وارد کنید").notEmpty().isString()
+],async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({data: null, errors: errors, message: 'Have Error',yourdata:req.body});
+    }
+
+    const news = await NewsModel.findByIdAndUpdate(req.headers.update_news_id, {
+        title: req.body.title,
+        images: req.body.images,
+        text1: req.body.text1,
+        text2: req.body.text2
+    },{new: true});
+    
     res.json({
-        data: news
+        "msg": "Update successfull !"
     })
 })
-
 module.exports.News = app
